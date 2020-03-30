1360c84ad4ecf5e32941fc7fcfbbd0568dcc1524
cantstopcoding <jonathan_koss@yahoo.com>
2020-03-30 00:12:23 -0400

    set up pry

--- lib/environment.rb
+++ lib/environment.rb
@@ -1,6 +1,8 @@
 require_relative "./ten_best_books/version"
 require_relative "./ten_best_books/cli"
 
+require 'pry'
+
 module TenBestBooks
   class Error < StandardError; end
   # Your code goes here...
