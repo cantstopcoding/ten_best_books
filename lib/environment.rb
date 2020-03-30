daab196eb619268853b3974d90ee7b24c41eb419
cantstopcoding <jonathan_koss@yahoo.com>
2020-03-29 22:46:08 -0400

    required relatives in environment file and modifying call method

--- lib/environment.rb
+++ lib/environment.rb
@@ -1,5 +1,5 @@
-require "ten_best_books/version"
-# require "ten_best_books/cli"
+require_relative "./ten_best_books/version"
+require_relative "./ten_best_books/cli"
 
 module TenBestBooks
   class Error < StandardError; end
--- lib/ten_best_books/cli.rb
+++ lib/ten_best_books/cli.rb
@@ -1,5 +1,6 @@
 class TenBestBooks::CLI
     def call 
-        puts "Welcome! Here are the 10 Best Books, Chosen by 125 Famous Authors..."
+        puts "Welcome!" 
+        puts "Here are the 10 Best Books Chosen by 125 Famous Authors..."
     end
 end 
